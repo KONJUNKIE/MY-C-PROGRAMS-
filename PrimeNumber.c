623aa59b5b26d053479d5ea9f16ba676ff19850b
KONJUNKIE <118102749+KONJUNKIE@users.noreply.github.com>
2022-11-17 19:48:34 +0800

    Update PrimeNumber.c

--- PrimeNumber.c
+++ PrimeNumber.c
@@ -1,34 +1,40 @@
 /******************************************************************************
+Name: Clint Joseph S. Ubanan
+
 C program that accepts a number n, and determine if the number n is a prime number.
 ******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int n;
+    int n, i;
     int count = 0;
     
-    printf("Input a number => ");
-    scanf("%d",&n);
-    
-    if(n == 1 || n == 0)
+    while(i)
     {
-        printf("The number %d is neither prime nor composite",n);
-        return 0;
-    }
-    
-    for(int i = 2; i <= n; i++)
+        printf("Input a number => ");
+        scanf("%d",&n);
+        
+        if(n == 1 || n == 0)
         {
-            if(n % i == 0)
-                count++;
+            printf("The number %d is neither prime nor composite",n);
         }
-    if(count == 1)
-    {
-        printf("The number %d is a Prime number",n);
-    }
-    else
-    {
-        printf("The number %d is not a Prime number",n);
+        
+        for(i = 2; i <= n; i++)
+            {
+                if(n % i == 0)
+                    count++;
+            }
+        if(count == 1)
+        {
+            printf("The number %d is a Prime number",n);
+        }
+        else
+        {
+            printf("The number %d is not a Prime number",n);
+        }
+        
+        printf("\n");
     }
     
     return 0;
